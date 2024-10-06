6aacbeb599cc66e69555b406a78f2ee0ebaea4e1
Tim van Brederode <timvanbrederode@live.nl>
2024-10-06 10:22:50 +0200

    Hive stockpile + ammo is being delivered correctly

--- Scenes/Locations/hive.gd
+++ Scenes/Locations/hive.gd
@@ -10,8 +10,28 @@ var spawn_interval = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	## Starting resources:
+	stockpile = [10, 10, 0, 25]
+	GameflowManager.stockpile_buildings.append(self)
+	GameflowManager.ammo_buildings.append(self)
+	
+func origin_action(ant : Ant, delta : float):
+	if ant.task.destination.is_tower:
+		if ant.task.wood and stockpile[2] > 0:
+			ant.add_to_inventory(CarryingResource.new(CarryingResource.ResourceType.WOODAMMO))
+			stockpile[2] -= 1
+		elif !ant.task.wood and stockpile[3] > 0:
+			ant.add_to_inventory(CarryingResource.new(CarryingResource.ResourceType.STONEAMMO))
+			stockpile[3] -= 1
+	else:
+		if ant.task.wood and stockpile[0] > 0:
+			ant.add_to_inventory(CarryingResource.new(CarryingResource.ResourceType.WOOD))
+			stockpile[0] -= 1
+		elif !ant.task.wood and stockpile[1] > 0:
+			ant.add_to_inventory(CarryingResource.new(CarryingResource.ResourceType.STONE))
+			stockpile[1] -= 1
+			
+	return true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
